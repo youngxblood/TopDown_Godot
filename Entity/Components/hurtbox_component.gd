a83bdb006258a03cc790003664acce4464959914
youngxblood <crazyeight68@gmail.com>
2023-08-08 13:33:30 -0700

    Added tree sprites, more setup

--- Entity/Components/hurtbox_component.gd
+++ Entity/Components/hurtbox_component.gd
@@ -7,7 +7,6 @@ signal hit_by_bullet(bullet: Bullet)
 @export var health_component : HealthComponent
 
 
-
 func can_accept_bullet_collision():
 	if health_component && health_component.owner_is_dead:
 		return true;
@@ -17,3 +16,8 @@ func can_accept_bullet_collision():
 
 func handle_bullet_collision(bullet : Bullet):
 	hit_by_bullet.emit(bullet);
+
+
+func _on_area_entered(other_area):
+	if other_area is HitboxComponent:
+		print_debug("Hit a hitbox");
