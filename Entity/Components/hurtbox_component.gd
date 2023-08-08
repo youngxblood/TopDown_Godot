aeba5c6dd3a226cb71ad5468b82593bdc49b35aa
youngxblood <crazyeight68@gmail.com>
2023-08-08 10:47:32 -0700

    Component updates, new character art and base

--- Entity/Components/hurtbox_component.gd
+++ Entity/Components/hurtbox_component.gd
@@ -2,4 +2,18 @@ extends Area2D
 
 class_name HurtboxComponent
 
+signal hit_by_bullet(bullet: Bullet)
+
 @export var health_component : HealthComponent
+
+
+
+func can_accept_bullet_collision():
+	if health_component && health_component.owner_is_dead:
+		return true;
+	else:
+		return false;
+
+
+func handle_bullet_collision(bullet : Bullet):
+	hit_by_bullet.emit(bullet);
---
+++ Entity/Player/player_character.gd
@@ -0,0 +1,2 @@
+extends "res://Entity/base_character.gd"
+
