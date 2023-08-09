ecc8cfcde2a8e3c01578e454877d0f77483f96f2
youngxblood <crazyeight68@gmail.com>
2023-08-09 01:12:53 -0700

    Need to fix anims, movement works

--- Entity/Player/player_character.gd
+++ Entity/Player/player_character.gd
@@ -1,2 +1,21 @@
 extends "res://Entity/base_character.gd"
 
+@export var speed : float = 300;
+var current_velocity : Vector2 = Vector2.ZERO
+
+
+func _physics_process(delta):
+	read_input()
+	update_animations()
+	move_and_slide()
+
+func read_input():
+	var move_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = move_direction * speed
+
+
+func update_animations():
+	if velocity == Vector2.ZERO:
+		anim_player.play("Idle")
+	if velocity != Vector2.ZERO:
+		anim_player.play("Run")
--- Entity/base_character.gd
+++ Entity/base_character.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
