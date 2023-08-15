07eeabdf3f86cd90593dd240330ea8b0001bdb61
youngxblood <crazyeight68@gmail.com>
2023-08-14 18:46:28 -0700

    Projectiles are destroyed after delay, added throw cooldown

--- Entity/Components/ProjectileComponents/projectilethrower_component.gd
+++ Entity/Components/ProjectileComponents/projectilethrower_component.gd
@@ -1,11 +1,19 @@
 extends Node2D
 class_name ProjectileThrowerComponent
 
+@onready var cooldown_timer : Timer = $CooldownTimer
+
 @export_file var projectile_location
 var string_to_load : String
+var is_off_cooldown : bool = true
+var cooldown_time : float = 0.2
 
 
+## Spawns the projectile when available
 func spawn_projectile():
+	if !is_off_cooldown: # Early return if on cooldown
+		return
+
 	string_to_load = projectile_location
 	var PROJECTILE = load(projectile_location)
 	if PROJECTILE:
@@ -19,3 +27,15 @@ func spawn_projectile():
 
 		var projectile_rotation = self.get_parent().global_position.direction_to(get_global_mouse_position()).angle()
 		projectile.rotation = projectile_rotation;
+		start_cooldown()
+
+
+## Start cool down in between spawn_projectile()
+func start_cooldown():
+	is_off_cooldown = false
+	cooldown_timer.wait_time = cooldown_time
+	cooldown_timer.start()
+	await cooldown_timer.timeout
+
+	# When timer is done
+	is_off_cooldown = true
--- Entity/Projectiles/projectile_base.gd
+++ Entity/Projectiles/projectile_base.gd
@@ -2,11 +2,18 @@ extends Node2D
 class_name ProjectileBase	
 
 @onready var sprite : Sprite2D = $Sprite2D
+@onready var lifetime_timer : Timer = $Lifetime_Timer
 
 @export var hitbox_component : HitboxComponent
+@export var timeout_time : float = 3
 
 var projectile_owner : BaseCharacter # Used to ensure owner does not get hit by projectile
 var projectile_speed = 300
+
+
+func _ready():
+	start_projectile_timeout()
+
 
 func _physics_process(delta):
 	var direction : Vector2 = Vector2.RIGHT.rotated(rotation);
@@ -15,3 +22,12 @@ func _physics_process(delta):
 
 func destroy_projectile():
 	self.queue_free()
+
+
+func start_projectile_timeout():
+	lifetime_timer.wait_time = timeout_time
+	lifetime_timer.start()
+	await lifetime_timer.timeout
+
+	# When timer is over
+	destroy_projectile()
